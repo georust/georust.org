b615ceae35fa944ce8dc6257c0fcdfffa2e8b905
Payas <payasrajan@live.com>
2020-09-01 20:35:12 -0700

    Github pages doesn't support public/, reverting to docs/

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ module.exports = {
     require('autoprefixer'),
     require('@fullhuman/postcss-purgecss')({
       content: [
-        './public/index.html'
+        './docs/index.html'
       ]
     })
   ]
